e975c8161829fd345a04f1e2a2899070b8a20e3b
ndipinto <nicola.dipinto@dxc.com>
2024-09-19 11:07:33 +0200

    init

---
+++ src/Card.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function Card() {
+
+    return (
+        <>
+            <h1>Cards</h1>
+            <img src="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?q=80&w=1794&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            <div>
+                <a href="https://youtube.com" > YouTube
+                </a>
+            </div>
+
+        </>
+    )
+}
+
+export default Card
